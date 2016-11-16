454aada1ce2c6fc18d77dc0ffcd45062c2f20126
Luis Rivera <web@luisriverag.com>
2016-11-16 12:16:48 +0100

    refactor-rename

--- app/src/main/java/com/mandrill/app/MainActivity.java
+++ app/src/main/java/com/mandrill/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.mandrill.app;
 
 import android.app.Activity;
 import android.os.Bundle;
--- app/src/main/java/com/mandrill/app/MyAppWebViewClient.java
+++ app/src/main/java/com/mandrill/app/MyAppWebViewClient.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.mandrill.app;
 
 import android.content.Intent;
 import android.net.Uri;
